559c93356d4604004b6e9988956adc4715094022
paulkiragu <123287301+paulkiragu@users.noreply.github.com>
2026-01-14 10:53:08 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+def add_numbers(a,b):
+  return a + b
+  print("Running the application...")
+  result = add_numbers(5, 5)
+
+if result == 10:
+  print("Test Passed: 5 + 5 is 10")
+  exit(0)
+
+else:
+  print("Test Failed!")
+  exit(1)
