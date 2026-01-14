7d5d98801457335447dda550dcb54a57610485e9
paulkiragu <123287301+paulkiragu@users.noreply.github.com>
2026-01-14 11:22:01 +0300

    Fix indentation and print statement in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def add_numbers(a, b):
   return a + b
-  print("Running the application...")
-  result = add_numbers(5, 5)
+print("Running the application...")
+result = add_numbers(5, 5)
 
 if result == 10:
   print("Test Passed: 5 + 5 is 10")
